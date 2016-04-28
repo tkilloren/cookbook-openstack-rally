880e5423db3fd06ea41392a4e04b8bdf793bb9d7
tkilloren <Travis.Killoren@target.com>
2016-04-28 12:28:38 -0500

    Clean up comments

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,61 +18,7 @@
 #
 
 
-#case node['platform_family']
-#when 'debian'
-#  include_recipe 'apt::default'
-#
-#  apt_repository 'pcs' do
-#    uri          node['pcs']['repo']['url']
-#    distribution 'binary/'
-#    key          node['pcs']['repo']['key']
-#  end
-#
-#  package 'pcs' do
-#    version node['pcs']['repo']['pkg_version']
-#  end
-#when 'rhel'
-#  include_recipe 'yum::default'
-#
-#  yum_repository 'pcs' do
-#    baseurl node['pcs']['repo']['url']
-#    gpgkey  node['pcs']['repo']['key']
-#  end
-#
-#  package 'pcs' do
-#    version node['pcs']['repo']['pkg_version']
-#  end
-#end
-
-#case node['platform_family']
-#when 'debian'
-#  include_recipe 'apt::default'
-#when 'rhel'
-#  include_recipe 'yum::default'
-#
-#  yum_repository 'rhel7-2015r1-tgt' do
-#    description 'Enterprise base'
-#    baseurl 'http://10.63.216.186:8000/rhel7-2015r1-tgt/'
-#    gpgcheck false
-#    action :create
-#  end
-#
-#  yum_repository 'rhel7-ha-2015r1-tgt' do
-#    description 'Enterprise HA addon'
-#    baseurl 'http://10.63.216.186:8000/rhel7-ha-2015r1-tgt/'
-#    gpgcheck false
-#    action :create
-#  end
-#
-#  yum_repository 'rhel7-ost6-2015r1-tgt' do
-#    description 'Enterprise OpenStack (juno)'
-#    baseurl 'http://10.63.216.186:8000/rhel7-ost6-2015r1-tgt/'
-#    gpgcheck false
-#    action :create
-#  end
-#
-  include_recipe 'yum-epel'
-#end
+include_recipe 'yum-epel'
 
 # Install Rally prerequisite packages
 %w(gcc libffi-devel python-devel openssl-devel gmp-devel libxml2-devel libxslt-devel postgresql-devel git wget python-pip redhat-rpm-config).each do |pkg|
@@ -86,30 +32,3 @@ execute 'Install rally from git' do
   creates '/usr/bin/rally'
   action :run
 end
-
-
-# Compute the hashed password in shadow format
-##FIXME -- this should be a helper-lib method
-#alg  = '$6$'
-#salt = 'EenedOKN' ##FIXME - generate random salt
-#hash = node['pcs']['hacluster_pass'].crypt(alg + salt)
-
-# Set password for hacluster user
-#user 'hacluster' do
-#  password hash
-#  action :modify
-#end
-
-# Start service that syncronizes the cluster configs
-#service "pcsd" do
-#  supports :restart => true, :reload => true, :status => true
-#  action [:enable, :start]
-#end
-
-# Enable corosync and pacemaker services
-#services = ['corosync', 'pacemaker']
-#services.each do |service_name|
-#  service service_name do
-#    action :enable
-#  end
-#end
